c10baefc448f5527837fcb02cea171c820133198
Paul Savchuk <nekrjkeee@gmail.com>
2023-05-17 00:07:00 +0300

    add chunking to parse.py

--- helpers/text_chunker.py
+++ helpers/text_chunker.py
@@ -1,14 +1,10 @@
-import os
-
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import TokenTextSplitter
 
 
-output_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "out",)
-text_filename = os.path.join(output_dir, "NiftyBridge.txt")
-with open(text_filename) as f:
-    state_of_the_union = f.read()
+def chunk_text_file(text_filename: str) -> list[str]:
+    with open(text_filename) as f:
+        state_of_the_union = f.read()
 
-text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=100, chunk_overlap=0)
-texts = text_splitter.split_text(state_of_the_union)
-
-print(texts[0])
+    text_splitter = TokenTextSplitter.from_tiktoken_encoder(chunk_size=128, chunk_overlap=0)
+    text_chunks = text_splitter.split_text(state_of_the_union)
+    return text_chunks
--- parse.py
+++ parse.py
@@ -1,6 +1,13 @@
+import os
+
 from core.settings import PDF_PATH
 from helpers.pdf_parser import convert_pdf_to_text
+from helpers.text_chunker import chunk_text_file
 
+
+output_file_location = os.path.join(os.path.dirname(os.path.abspath(__file__)), "helpers", "out", )
+text_filename = os.path.join(output_file_location, "NiftyBridge.txt")
 
 if __name__ == "__main__":
     convert_pdf_to_text(PDF_PATH)
+    text_chunks = chunk_text_file(text_filename)
